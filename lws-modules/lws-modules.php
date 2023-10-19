13dcebd218d6c503e94efbe4597ef68113073d47
liquiddev03 <48906904+liquiddev03@users.noreply.github.com>
2023-10-19 14:40:12 +0530

    Update lws-modules.php

--- lws-modules/lws-modules.php
+++ lws-modules/lws-modules.php
@@ -22,3 +22,15 @@ if (!defined('ABSPATH')) die('No direct access allowed');
  * Admin: Pages / Subpages
  */
 require_once plugin_dir_path( __FILE__ ) . 'admin/admin-pages.php';
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/iamsajidjaved/Auto-update-WordPress-plugin-or-Theme-from-Github',
+	__FILE__,
+	'FunPlugin'
+);
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');
+
+//Optional: If you're using a private repository, specify the access token like this:
+$myUpdateChecker->setAuthentication('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');
